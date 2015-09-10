797b09375ad527ba002f26a776962f58edad2cf9
singh <singh@DESKTOP-DIPUTC0>
2015-09-09 23:06:10 -0400

    Calculator has been committed

---
+++ Chapter 4/src/Calculator.java
@@ -0,0 +1,13 @@
+
+public class Calculator {
+	 // Instance variable
+	double result = 0;    
+	// Instance methods  
+	public void sum(int i, int k){      
+		result = i + k;  
+		}    
+	public void multiply(int i, int k) { 
+		result = i * k; 
+		} 
+
+}
---
+++ Chapter 4/src/CalculatorTest.java
@@ -0,0 +1,18 @@
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	      // First object 
+		Calculator cal1 = new Calculator(); 
+		
+		cal1.sum(4, 16); 
+		System.out.println(cal1.result); 
+		// Second object 
+		Calculator cal2 = new Calculator(); 
+		cal2.multiply(5, 6);  
+		System.out.println(cal2.result);
+		} 
+
+	}
+
